78a5e3f89bf139880e50e6006fdbff2b552487c7
AC-devon <inbox@avaulto.com>
2023-09-13 16:29:00 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ToolCard from './tool';
+
+import Box from '@mui/material/Box';
+
+import Grid from '@mui/material/Grid';
 
 function App() {
+  const apps = [{
+    name: 'stake reward history',
+    url: 'https://t-apy.solanahub.app',
+    desc: 'fetch all your stake rewards and view the true APY you received'
+  },
+  {
+    name: 'Devnet faucet',
+    url: 'https://dev-faucet.solanahub.app/',
+    desc: 'a quick solution for Solana devnet SOL and devnet NFT(SPL)'
+  },
+  {
+    name: 'liquid stake dashboard',
+    url: 'https://lst-ds-dashboard.solanahub.app',
+    desc: 'Direct stake dashboard for liquid stake asset such as mSOL and bSOL'
+  },
+  {
+    name: 'SolanaHub',
+    url: 'https://www.solanahub.app',
+    desc: 'The ultimate community-driven onboarding app for Solana users'
+  }
+  ]
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="app-show-case">
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={1}>
+            <Grid item xs={3}>
+              <ToolCard tool={apps[0]} />
+            </Grid>
+            <Grid item xs={3}>
+              <ToolCard tool={apps[1]} />
+            </Grid>
+            <Grid item xs={3}>
+              <ToolCard tool={apps[2]} />
+            </Grid>
+            <Grid item xs={3}>
+              <ToolCard tool={apps[3]} />
+            </Grid>
+          </Grid>
+        </Box>
+        {/* <div >
+
+        <div className='iframe-wrapper'>
+        <div className='overlay'></div>
+         <iframe className='tool' src={apps[1]} ></iframe>
+    </div>
+        </div> */}
+        {/* <iframe className='tool' src={apps[2]} ></iframe>
+          <iframe className='tool' src={apps[3]} ></iframe> */}
+
+      </div>
+      <span id="love">built with Love by <a href="https://www.SolanaHub.app" target="_blank">
+        <span id="logo">SolanaHub</span>
+        <img src={logo} alt="avaulto logo" /></a></span>
     </div>
   );
 }
---
+++ src/tool.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+  import Card from '@mui/material/Card';
+  import CardActions from '@mui/material/CardActions';
+  import CardContent from '@mui/material/CardContent';
+  import CardMedia from '@mui/material/CardMedia';
+  import Button from '@mui/material/Button';
+  import Typography from '@mui/material/Typography';
+
+  interface AppCard {
+    name: string;
+    url: string;
+    desc: string;
+  }
+export default function ToolCard ({tool}: any) {
+    console.log(tool)
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <div className='item'>
+
+          <div className='overlay'></div>
+          <iframe className='tool' src={tool.url} ></iframe>
+          </div>
+       
+      <CardContent>
+        <Typography style={{textTransform:'capitalize'}} gutterBottom variant="h5" component="div">
+          {tool.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {tool.desc}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <a target='_blank' href={tool.url}>
+        <Button size="small">Learn More</Button>
+        </a>
+      </CardActions>
+    </Card>
+  );
+}
